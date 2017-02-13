db2b39a050ceede2015236e65ea2f0e5ebd4f458
jSunnari <jsunnari@gmail.com>
2017-02-13 11:08:03 +0100

    Testing LCD-display

--- src/main/java/nu/sunnari/weatherpi/WeatherPiApplication.java
+++ src/main/java/nu/sunnari/weatherpi/WeatherPiApplication.java
@@ -2,11 +2,13 @@ package nu.sunnari.weatherpi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by Jonas on 2017-01-23.
  */
 
+@EnableScheduling
 @SpringBootApplication
 public class WeatherPiApplication {
     public static void main(String[] args) {
