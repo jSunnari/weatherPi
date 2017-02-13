6103d072b1d0490e428120e79bf0fb4795de2473
jSunnari <jsunnari@gmail.com>
2017-02-13 09:04:06 +0100

    Testing LCD-display

--- src/main/java/nu/sunnari/weatherpi/WeatherPiApplication.java
+++ src/main/java/nu/sunnari/weatherpi/WeatherPiApplication.java
@@ -10,13 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeatherPiApplication {
     public static void main(String[] args) {
-/*
+
         try {
             LcdDisplay lcdDisplay = new LcdDisplay();
         } catch (Exception e) {
             e.printStackTrace();
         }
-*/
+
 
         DataCollector dataCollector = new DataCollector();
         System.out.println(dataCollector.getCurrentDate());
