443176a1c8e0d34df1bcdbcfe97bc5b245e7a7d9
jSunnari <jsunnari@gmail.com>
2017-02-15 17:46:00 +0100

    Test sensors

--- src/main/java/nu/sunnari/weatherpi/LcdDisplay.java
+++ src/main/java/nu/sunnari/weatherpi/LcdDisplay.java
@@ -17,7 +17,7 @@ class LcdDisplay {
                     20,
                     I2CBus.BUS_1,
                     0x27,
-                    2,
+                    3,
                     0,
                     1,
                     2,
