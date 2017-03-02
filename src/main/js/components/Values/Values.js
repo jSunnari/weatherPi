3e57390dc0a35f499be80e2a93627a792bfec61e
jSunnari <jsunnari@gmail.com>
2017-03-02 15:31:17 +0100

    Connecting Current Values-site with backend.

--- src/main/js/components/Values/Values.js
+++ src/main/js/components/Values/Values.js
@@ -6,34 +6,14 @@ export default class Values extends Component {
 
     constructor(props){
         super(props);
-        this.state = {
-            outsideTemp: 0,
-            outsideTempMin: 0,
-            outsideTempMax: 0,
-            outsideHum: 0,
-            outsideHumMin: 0,
-            outsideHumMax: 0,
-            outsidePressure: 0,
-            outsidePressureMin: 0,
-            outsidePressureMax: 0,
-            insideTemp: 0,
-            insideTempMin: 0,
-            insideTempMax: 0,
-            insideHum: 0,
-            insideHumMin: 0,
-            insideHumMax: 0,
-            outsideTempTrend: "direction-right",
-            outsideHumTrend: "direction-right",
-            outsidePressureTrend: "direction-right",
-            insideTempTrend: "direction-right",
-            insideHumTrend: "direction-right"
-        }
+        this.state = ({
+            currentWeather: {}
+        })
     }
 
     componentWillMount(){
         serverRequest.getCurrentWeather().then((response) => {
-            console.log(response);
-            console.log(response.indoorTemp);
+            this.setState({currentWeather: response});
         }, (error) => {
             console.error(error);
         });
@@ -53,8 +33,8 @@ export default class Values extends Component {
 
                 <div id="outdoor-values-container">
                     <h1 className="location-header">Outdoor</h1>
-                    <CurrentValue iconName="thermometer" currentValue="4.8°C" minValue="-1°C" maxValue="4.9°C" arrow="direction-right"/>
-                    <CurrentValue iconName="humidity" currentValue="88%" minValue="79%" maxValue="99%" arrow="direction-up-right"/>
+                    <CurrentValue iconName="thermometer" currentValue={this.state.currentWeather.outdoorTemp}"°C" minValue={this.state.currentWeather.outdoorMinTemp}"°C" maxValue={this.state.currentWeather.outdoorMaxTemp}"°C" arrow={this.state.currentWeather.outdoorTempTrend}/>
+                    <CurrentValue iconName="humidity" currentValue={this.state.currentWeather.outdoorHum}"%" minValue={this.state.currentWeather.outdoorMinHum}"%" maxValue={this.state.currentWeather.outdoorMaxHum}"%" arrow={this.state.currentWeather.outdoorHumTrend}/>
                     <CurrentValue iconName="barometer" currentValue="1033hPa" minValue="1020hPa" maxValue="1030hPa" arrow="direction-down-right"/>
                 </div>
 
