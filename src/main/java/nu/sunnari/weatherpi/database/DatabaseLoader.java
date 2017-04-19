524350a9c94f31811fa07507cfed93dcd5eba1e2
jSunnari <jsunnari@gmail.com>
2017-04-19 10:25:55 +0200

    Testing new database-functions

--- src/main/java/nu/sunnari/weatherpi/database/DatabaseLoader.java
+++ src/main/java/nu/sunnari/weatherpi/database/DatabaseLoader.java
@@ -25,7 +25,7 @@ public class DatabaseLoader implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-
+/*
         for (int i = 1; i < 30; i++) {
             for (int j = 0; j < 24; j++) {
                 Random r = new Random();
@@ -40,7 +40,7 @@ public class DatabaseLoader implements CommandLineRunner {
                 );
             }
         }
-
+*/
     }
 
 }
