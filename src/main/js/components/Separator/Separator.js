57f3029d4ac5a02638a674b3cc809a70001703c5
jSunnari <jsunnari@gmail.com>
2017-05-26 15:11:18 +0200

    iPad-view, including separators

---
+++ src/main/js/components/Separator/Separator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Separator extends Component {
+
+    render() {
+        return (
+            <div className="hr-div">
+                <hr className="left-hr" />
+                <hr className="right-hr" />
+            </div>
+        )
+    }
+}
--- src/main/js/components/Values/Values.js
+++ src/main/js/components/Values/Values.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CurrentValue from '../CurrentValue/CurrentValue';
+import Separator from '../Separator/Separator'
 import { serverRequest } from '../../serverRequest';
 
 import './Values.scss';
@@ -82,13 +83,16 @@ export default class Values extends Component {
                 <div id="outdoor-values-container">
                     <h1 className="location-header">OUTDOOR</h1>
                     <CurrentValue iconName="thermometer" currentValue={this.state.currentWeather.outdoorTemp + "°C"} minValue={this.state.currentWeather.outdoorMinTemp + "°C"} maxValue={this.state.currentWeather.outdoorMaxTemp + "°C"} arrow={this.state.currentWeather.outdoorTempTrend} />
+                    <Separator/>
                     <CurrentValue iconName="humidity" currentValue={this.state.currentWeather.outdoorHum + "%"} minValue={this.state.currentWeather.outdoorMinHum + "%"} maxValue={this.state.currentWeather.outdoorMaxHum + "%"} arrow={this.state.currentWeather.outdoorHumTrend} />
+                    <Separator/>
                     <CurrentValue iconName="barometer" currentValue={this.state.currentWeather.outdoorPressure + "hPa"} minValue={this.state.currentWeather.outdoorMinPressure + "hPa"} maxValue={this.state.currentWeather.outdoorMaxPressure + "hPa"} arrow={this.state.currentWeather.outdoorPressureTrend} />
                 </div>
 
                 <div id="indoor-values-container">
                     <h1 className="location-header">INDOOR</h1>
                     <CurrentValue iconName="thermometer" currentValue={this.state.currentWeather.indoorTemp + "°C"} minValue={this.state.currentWeather.indoorMinTemp + "°C"} maxValue={this.state.currentWeather.indoorMaxTemp + "°C"} arrow={this.state.currentWeather.indoorTempTrend}/>
+                    <Separator/>
                     <CurrentValue iconName="humidity" currentValue={this.state.currentWeather.indoorHum + "%"} minValue={this.state.currentWeather.indoorMinHum + "%"} maxValue={this.state.currentWeather.indoorMaxHum + "%"} arrow={this.state.currentWeather.indoorHumTrend}/>
                 </div>
 
